d33756173a4f5431acae83cc6485f0efc9d61043
Fady John <63600279+FadyJohn10@users.noreply.github.com>
2023-09-23 22:09:08 +0400

    summarizer added

--- chat.py
+++ chat.py
@@ -1,10 +1,10 @@
 import random
 import json
-
 import torch
-
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
+
+from summarizer import summarize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -25,29 +25,32 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "ur fav bot"
+bot_name = "Bolt"
 print("Let's chat! (type 'quit' to exit)")
 while True:
-    # sentence = "do you use credit cards?"
     sentence = input("You: ")
     if sentence == "quit":
         break
 
-    sentence = tokenize(sentence)
-    X = bag_of_words(sentence, allWords)
-    X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
-
-    output = model(X)
-    _, predicted = torch.max(output, dim=1)
-
-    tag = tags[predicted.item()]
-
-    probs = torch.softmax(output, dim=1)
-    prob = probs[0][predicted.item()]
-    if prob.item() > 0.75:
-        for intent in intents['intents']:
-            if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    if sentence == "summarize":
+        text = input("provide the text: ")
+        summarize(text)
     else:
-        print(f"{bot_name}: I do not understand...")
+        sentence = tokenize(sentence)
+        X = bag_of_words(sentence, allWords)
+        X = X.reshape(1, X.shape[0])
+        X = torch.from_numpy(X).to(device)
+
+        output = model(X)
+        _, predicted = torch.max(output, dim=1)
+
+        tag = tags[predicted.item()]
+
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
+        if prob.item() > 0.85:
+            for intent in intents['intents']:
+                if tag == intent["tag"]:
+                    print(f"{bot_name}: {random.choice(intent['responses'])}")
+        else:
+            print(f"{bot_name}: I do not understand...")
